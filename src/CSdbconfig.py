bec78e6a19af19cba037fb351d169eeadf691f84
David Stockton <davidbrucestocktonbme@gmail.com>
2017-04-27 12:34:24 -0500

    Remove personal info from config file.

---
+++ src/CSdbconfig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# import preprocessing
+mysql = {'host': 'localhost',
+         'user': 'root',
+         'password': 'xxx'}
+# preprocessing_queue = [preprocessing.scale_and_center,
+#                        preprocessing.dot_reduction,
+#                        preprocessing.connect_lines]
+use_anonymous = True
