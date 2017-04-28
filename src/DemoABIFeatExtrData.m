566c2262525660cb70c86524b3afd03ec63b59c9
David.Stockton <eod691@my.utsa.edu>
2017-04-28 09:20:46 -0500

    Change name of config file

--- src/DemoABIFeatExtrData.m
+++ src/DemoABIFeatExtrData.m
@@ -1,11 +1,12 @@
 % DemoABIFeatExtrData
-% Illustrates use of ABIFeatExtrData MATLAB utility
+% Illustrates use of ABIFeatExtrData MATLAB utility.
+% Utilities are simple, which provides flexibility.
 
 clc
 clear classes;
 
 %% Connect with the experimental data (ABI) database
-config = ini2struct('dbsconfig.ini');
+config = ini2struct('config.ini');
 username = config.CellSurvey.mySQLUsername;
 password = config.CellSurvey.mySQLPassword;
 
@@ -14,10 +15,10 @@ abiDatabaseName = 'ABICellSurvey';
 disp(['Connecting to ABI database ' abiDatabaseName '.']);
 abiDBConn = database.ODBCConnection(abiDatabaseName, username, password);
 
-afed = ABIFeatExtrData(abiDBConn)
+afed = ABIFeatExtrData(abiDBConn);
 
-specNum = 469753383
-expNum = 48
+specNum = 469753383;
+expNum = 48;
 
 expData = afed.getExpFXData(specNum, expNum);
 
